daeb0dae32c0e5ad711891a30c375da7ced0425b
Gauvain01 <79645236+Gauvain01@users.noreply.github.com>
2023-12-18 17:00:51 +0100

    +sub/unsub methods to input bus

--- globals/input_bus.gd
+++ globals/input_bus.gd
@@ -5,8 +5,7 @@ signal mouseClick
 signal mouseRelease
 signal rightMouseClick
 
-
-var saasd
+var SUBSCRIBED_NODES = {}; #{node_ref: [{"signal":signal_ref, "callable":callable_ref}]}
 
 
 
@@ -38,3 +37,52 @@ func _unsubscribe_from_signal_array(signal_array:Array, callable:Callable)->void
 		var sig_call:Callable = sig_dic["callable"]
 		if callable == sig_call:
 			sig.disconnect(sig_call)
+
+
+func _append_subscribed_node_to_signal(observing_node:Node, signal_ref:Signal, callable:Callable):
+	if SUBSCRIBED_NODES.has(observing_node):
+		var signal_arr =SUBSCRIBED_NODES[observing_node]
+		signal_arr.append({"signal":signal_ref, "callable":callable})
+		return
+
+	SUBSCRIBED_NODES[observing_node] = ({"signal":signal_ref, "callable":callable})
+	
+func disconnect_all_from_node(node:Node):
+	if !SUBSCRIBED_NODES.has(node):
+		return
+	if len(SUBSCRIBED_NODES[node]) == 0:
+		return
+	for signal_callable_map in SUBSCRIBED_NODES[node]:
+		signal_callable_map["signal"].disconnect(signal_callable_map["callable"])
+	SUBSCRIBED_NODES[node] = []
+
+func unsubscribe_from_signal(observing_node:Node, signal_ref:Signal):
+	if !SUBSCRIBED_NODES.has(observing_node):
+		return
+	if len(SUBSCRIBED_NODES[observing_node]) == 0:
+		return
+	for signal_callable_map in SUBSCRIBED_NODES[observing_node]:
+		if signal_callable_map["signal"] == signal_ref:
+			signal_callable_map["signal"].disconnect(signal_callable_map["callable"])
+
+func subscribe_to_signal(observing_node:Node, signal_ref:Signal, callable:Callable):
+	_append_subscribed_node_to_signal(observing_node, signal_ref, callable)
+	signal_ref.connect(callable)
+func subscribe_to_mouse_click_event(observing_node:Node, callable:Callable):
+	subscribe_to_signal(observing_node, mouseClick, callable)
+
+func subscribe_to_mouse_release_event(observing_node:Node, callable:Callable):
+	subscribe_to_signal(observing_node, mouseRelease, callable)
+
+func subscribe_to_space_pressed_event(observing_node:Node, callable:Callable):
+	subscribe_to_signal(observing_node, spacePressed, callable)
+
+func subscribe_to_right_mouse_click_event(observing_node:Node, callable:Callable):
+	subscribe_to_signal(observing_node, mouseClick, callable)
+
+
+
+
+
+
+
