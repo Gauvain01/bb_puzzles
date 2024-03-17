0ef83f8abda6416a3881a865fbac352e4eaa6543
Gauvain01 <steijejodh@gmail.com>
2024-03-17 11:53:33 +0100

    +setup_puzzle_generator

---
+++ puzzle_generator/puzzle_generator.gd
@@ -0,0 +1,17 @@
+class_name PuzzleGenerator
+extends Node
+
+
+##generate puzzle from puzzle data
+func generate(puzzle_data:PuzzleData) -> Node:
+	#check_puzzle_type ->
+	#instantiate relevant puzzle 
+	#load players from file
+	#load players in to team
+	#check who has the ball and where is the ball
+	#attach relevant node connections if necessary
+	#maybe do some validation??
+	#attach a win condition
+	#connect win condition
+	#return node 
+	pass	
